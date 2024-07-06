1e4fa0ca0957b37cd6dcead39207b3d1b60aa1b5
KadirToptas <kdrrtptss@gmail.com>
2024-07-06 19:25:11 +0300

    Save-Load-with-json

--- Assets/Scripts/CharacterManager.cs
+++ Assets/Scripts/CharacterManager.cs
@@ -1,18 +1,64 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
 
 public class CharacterManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Characters characters;
+
+
+    private void Awake()
     {
-        
+        LoadCharacterData();
     }
 
-    // Update is called once per frame
-    void Update()
+[ContextMenu("Update")]
+    public void UpdateCharacter()
     {
-        
+        characters.characterDatas[0].Name = "Alp";
+        characters.characterDatas[0].Element = "Tahta";
+        characters.characterDatas[0].Damage = 10f;
+        characters.characterDatas[0].Health = 150f;
+        Save();
+
     }
+
+
+    #region Save&Load
+
+    private void LoadCharacterData()
+    {
+        if (File.Exists(Application.persistentDataPath + "/CharacterData.json"))
+        {
+            Load();
+        }
+    }
+    
+    [ContextMenu("Save")]
+    public void Save()
+    {
+        string json = JsonUtility.ToJson(characters, true);
+        File.WriteAllText(Application.persistentDataPath + "/CharacterData.json",json);
+    }
+
+
+
+    public void Load()
+    {
+        string json = File.ReadAllText(Application.persistentDataPath + "/CharacterData.json");
+        characters = JsonUtility.FromJson<Characters>(json);
+    }
+
+    #endregion
+}
+
+
+
+[System.Serializable]
+
+public class Characters
+{
+    public List<CharacterData> characterDatas = new List<CharacterData>();
 }
